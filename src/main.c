13837687422b0b93e46dc07414f407bd3e50540c
Andre Debuisne <andre.debuisne@epita.fr>
2018-11-09 22:32:08 +0100

    Created main function with SDL window creation

---
+++ src/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include <SDL2/SDL.h>
+
+int main(int argc, char *argv[])
+{
+    int height = 800;
+    int width = 600;
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window  = SDL_CreateWindow("<GAME NAME>", SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_OPENGL);
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1,
+        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    bool running = true;
+    SDL_Event event;
+    while (running)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_KEYDOWN)
+            {
+                printf("User typed something\n");
+                //printf("Key typed is %d\n", event.key.sym);
+            }
+            if (event.type == SDL_QUIT)
+            {
+                running = false;
+            }
+        }
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_Delay(2000);
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_Quit();
+    return 0;
+}
